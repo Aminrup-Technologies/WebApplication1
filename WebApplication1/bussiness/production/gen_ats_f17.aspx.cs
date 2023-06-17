1d4c06a8bde05cc7f36e973e31155ea5e46740b9
Purnima <punambhakat212@gmail.com>
2023-06-17 11:23:04 +0530

    Oldpc changes

--- WebApplication1/bussiness/production/gen_ats_f17.aspx.cs
+++ WebApplication1/bussiness/production/gen_ats_f17.aspx.cs
@@ -233,7 +233,7 @@ namespace WebApplication1.bussiness.production
             SqlCommand cmd = new SqlCommand(query, dbcl.Conn);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             da.Fill(dt_emps);
-            
+
 
             //SqlCommand cmd = new SqlCommand(query, dbcl.Conn);
             //SqlDataAdapter ad = new SqlDataAdapter(cmd);
@@ -433,7 +433,7 @@ namespace WebApplication1.bussiness.production
 
 
                 //---------------- OT Pay -------- Gross Rate
-                decimal otpay = 0.0m;    
+                decimal otpay = 0.0m;
                 decimal wrkhrs = Convert.ToDecimal(lbl_WorkHours.Text.ToString());
                 decimal otdiv = Convert.ToDecimal(lbl_OT_Divisibility.Text.ToString());  //Added on 29-11-2021
                 decimal otfactor = Convert.ToDecimal(lbl_OTFactor.Text.ToString());
